06640f0640f205c921d6f8deea69ad4a341be3e5
Conor Landry <clandry94@ufl.edu>
2017-10-18 13:37:39 -0400

    add seed for press release

--- config/env/development.js
+++ config/env/development.js
@@ -118,6 +118,24 @@ module.exports = {
           content: 'This is a seeded Article for the development environment'
         }
       }]
+    }, {
+      model: 'Release',
+      options: {
+        // Override log results setting at the
+        // collection level.
+        logResults: true
+      },
+      skip: {
+        // Skip collection when this query returns results.
+        // e.g. {}: Only seeds collection when it is empty.
+        when: {} // Mongoose qualified query
+      },
+      docs: [{
+        data: {
+          title: 'First Press Release',
+          content: 'This is a seeded Press Release for the development environment'
+        }
+      }]
     }]
   }
 };
