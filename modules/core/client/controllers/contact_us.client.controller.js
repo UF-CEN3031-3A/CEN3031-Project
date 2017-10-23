e01dc1fd7d2c6f4b4ce2d69c099454dfb46e19d9
Tess <tesshbianchi@gmail.com>
2017-10-23 13:41:09 -0400

    business inquiry, removes nav bar in contact us, fixes linting errors

--- modules/core/client/controllers/contact_us.client.controller.js
+++ modules/core/client/controllers/contact_us.client.controller.js
@@ -1,20 +1,22 @@
-(function() {
-    'use strict';
+(function () {
+  'use strict';
 
-    angular.module('core').controller('ContactUsController', ContactUsController);
+  angular.module('core').controller('ContactUsController', ContactUsController);
 
-    function ContactUsController($scope)
-    {
-        var vm = this;
+  function ContactUsController($scope) {
+    var vm = this;
 
-        $scope.oneAtATime = false;
+    $scope.oneAtATime = false;
+    $scope.groups = [
+    { title: 'FAQ 1', content: 'Dynamic Group Body - 1' },
+    { title: 'FAQ 2', content: 'Dynamic Group Body - 2' }];
 
-        $scope.groups = [
-            {title: 'FAQ 1', content: 'Dynamic Group Body - 1'},
-            {title: 'FAQ 2', content: 'Dynamic Group Body - 2'}
-        ];
+    $scope.status = {
+      isCustomHeaderOpen: false,
+      isFirstOpen: true,
+      isFirstDisabled: false
+    };
 
-        $scope.status = {isCustomHeaderOpen: false, isFirstOpen: true, isFirstDisabled: false};
-    }
+  }
 
 }());
