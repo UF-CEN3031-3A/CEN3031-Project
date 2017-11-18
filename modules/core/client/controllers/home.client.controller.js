1856efa0e28ca2eaf86c647bfca59801542dc287
Matthew <mavellanosa@ufl.edu>
2017-11-18 16:01:35 -0500

    Linked backend of Home page to front end. Modified Home page route

--- modules/core/client/controllers/home.client.controller.js
+++ modules/core/client/controllers/home.client.controller.js
@@ -3,18 +3,13 @@
 
   angular
     .module('core')
-    .controller('HomeController', HomeController);
+    .controller('HomeControll', HomeControll);
 
-  function HomeController($scope) {
+  function HomeControll($scope) {
 
     var vm = this;
 
     $scope.oneAtATime = false;
-
-    $scope.groups = [
-        { title: 'Lorem Ipsum', author: 'Mr. Ipsum', content: 'Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum' },
-        { title: 'Lorem Ipsum', author: 'Mr. Ipsum', content: 'Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum' }
-    ];
 
     $scope.status = { isCustomHeaderOpen: false, isFirstOpen: true, isFirstDisabled: false };
   }
---
+++ modules/releases/client/controllers/home.client.controller.js
@@ -0,0 +1,31 @@
+(function () {
+  'use strict';
+
+  angular
+    .module('releases')
+    .controller('HomeController', HomeController);
+
+  HomeController.$inject = ['ReleasesService'];
+
+  function HomeController(ReleasesService) {
+
+    var vm = this;
+
+    vm.oneAtATime = false;
+
+    vm.releases = ReleasesService.query();
+
+    //Variables to hold text for different titles and blurbs in the view
+    vm.bannerText = 'Just One Drop.';
+    vm.ParallaxCaption1 = 'Introducing: t*Rez';
+    vm.Heading1 = 'Your Next Source of Antioxidant Benefits';
+    vm.Heading2 = 'Now Making its Way in to All Your Favorite Products';
+    vm.mainText1 = 'This is a blurb that serves to briefly summarize the initial product video featured above, and reaffirm the impact and power of this product to bring the heart-healthy, antioxidant-based benefits of resveratrol into the new t*Rez solution. This blurb should entice the reader to continue down the page to explore different artifacts (articles, releases, studies, etc).';
+    vm.ParallaxCaption2 = 'In The News';
+    vm.ReleasesHeading = 'This Just In';
+    vm.ReleasesText = 'This is a blurb that introduces what the world is saying about antioxidants, resveratrol, and t*Rez.';
+
+    
+    vm.status = { isCustomHeaderOpen: false, isFirstOpen: true, isFirstDisabled: false };
+  }
+}());
